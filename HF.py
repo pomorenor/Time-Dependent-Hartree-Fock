50bace5c294444e1ed00a56d908fdd3d94116795
pomorenor <pomorenor@unal.edu.co>
2022-10-30 11:27:14 -0500

    The xyz file is added and the implementation for reading it

--- HF.py
+++ HF.py
@@ -2,3 +2,20 @@ import numpy as np
 import scipy
 from scipy.special import erf
 from numpy import *
+import pandas as pd
+
+def extract_xyz(data_file):
+
+    df = pd.read_csv(data_file, skiprows = 2,
+     delim_whitespace= True, names=['atom', 'x', 'y', 'z'])
+
+    atom_type = [i for i in df["atom"]]
+    atom_coordinates = [(i,j,k) for i,j,k in zip(df["x"],df["y"],df["z"])]
+
+
+
+
+    return atom_type, atom_coordinates
+
+
+print(extract_xyz('HeH.xyz'))
